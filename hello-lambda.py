99a159470752ac0a8975e9633e5305747e59ffb1
uvidit <uvidit@gmail.com>
2022-08-13 17:31:26 +0300

    added echo feature to bot, updated requirements.txt

--- hello-lambda.py
+++ hello-lambda.py
@@ -1,8 +1,16 @@
-import json
-
+import json, os
+from botocore.vendored import requests
+TELE_TOKEN=os.environ.get('TOKEN')
+URL = "https://api.telegram.org/bot{}/".format(TELE_TOKEN)
+def send_message(text, chat_id):
+    final_text = "You said: " + text
+    url = URL + "sendMessage?text={}&chat_id={}".format(final_text, chat_id)
+    requests.get(url)
 def lambda_handler(event, context):
-    print("hi there from lambda!")
+    message = json.loads(event['body'])
+    chat_id = message['message']['chat']['id']
+    reply = message['message']['text']
+    send_message(reply, chat_id)
     return {
-        'statusCode': 200,
-        'body': json.dumps('hi there from lambda!')
+        'statusCode': 200
     }
