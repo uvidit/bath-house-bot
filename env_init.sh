b0deeca11d2357f573bc981c196871b7973206a9
uvidit <uvidit@gmail.com>
2022-08-13 19:47:56 +0300

    added echo feature to bot, updated requirements.txt

--- env_init.sh
+++ env_init.sh
@@ -6,11 +6,17 @@ PY_LAMBDA_FILE_NAME='hello-lambda.py'
 
 rm -rf ${PYTHON_VIRTUAL_ENV_NAME} ${PY_LAMBDA_DEPENDENCIES} ${PY_LAMBDA_ZIP_NAME}
 
-python -m pip install virtualenv
-python -m venv ${PYTHON_VIRTUAL_ENV_NAME}
-source ./${PYTHON_VIRTUAL_ENV_NAME}/bin/activate
-pip install -r requirements.txt
 mkdir $PY_LAMBDA_DEPENDENCIES
-cp -r ./${PYTHON_VIRTUAL_ENV_NAME}/lib/*/site-packages/* ./${PY_LAMBDA_DEPENDENCIES}
+pip install --target ./$PY_LAMBDA_DEPENDENCIES -r requirements.txt
+
 zip -r ${PY_LAMBDA_ZIP_NAME} ./${PY_LAMBDA_DEPENDENCIES}/.
 zip -g ${PY_LAMBDA_ZIP_NAME} ${PY_LAMBDA_FILE_NAME}
+
+#python -m pip install virtualenv
+#python -m venv ${PYTHON_VIRTUAL_ENV_NAME}
+#source ./${PYTHON_VIRTUAL_ENV_NAME}/bin/activate
+#pip install -r requirements.txt
+#mkdir $PY_LAMBDA_DEPENDENCIES
+#cp -r ./${PYTHON_VIRTUAL_ENV_NAME}/lib/*/site-packages/* ./${PY_LAMBDA_DEPENDENCIES}
+#zip -r ${PY_LAMBDA_ZIP_NAME} ./${PY_LAMBDA_DEPENDENCIES}/.
+#zip -g ${PY_LAMBDA_ZIP_NAME} ${PY_LAMBDA_FILE_NAME}
