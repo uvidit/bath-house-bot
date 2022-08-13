5a276e9591da6e639f129d1aa540a30250c85c45
uvidit <uvidit@gmail.com>
2022-08-13 18:56:34 +0300

    added echo feature to bot, updated requirements.txt

--- hello-lambda.py
+++ hello-lambda.py
@@ -1,16 +1,34 @@
-import json, os
-from botocore.vendored import requests
-TELE_TOKEN=os.environ.get('TOKEN')
-URL = "https://api.telegram.org/bot{}/".format(TELE_TOKEN)
-def send_message(text, chat_id):
-    final_text = "You said: " + text
-    url = URL + "sendMessage?text={}&chat_id={}".format(final_text, chat_id)
-    requests.get(url)
-def lambda_handler(event, context):
-    message = json.loads(event['body'])
-    chat_id = message['message']['chat']['id']
-    reply = message['message']['text']
-    send_message(reply, chat_id)
-    return {
-        'statusCode': 200
-    }
+import os, logging
+from aiogram import Bot, Dispatcher, executor, types
+
+TOKEN=os.environ.get('TOKEN')
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler()
+async def lambda_handler(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+
+
+# URL = "https://api.telegram.org/bot{}/".format(TELE_TOKEN)
+# def send_message(text, chat_id):
+#     final_text = "You said: " + text
+#     url = URL + "sendMessage?text={}&chat_id={}".format(final_text, chat_id)
+#     requests.get(url)
+# def lambda_handler(event, context):
+#     message = json.loads(event['body'])
+#     chat_id = message['message']['chat']['id']
+#     reply = message['message']['text']
+#     send_message(reply, chat_id)
+#     return {
+#         'statusCode': 200
+#     }
