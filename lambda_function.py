f253ce7041f6cc87f29a6ddb51d31eed1d1b66f4
uvidit <uvidit@gmail.com>
2022-08-16 02:00:24 +0300

    sync dev with master (#5)
    * added more answers on where Jason is now, added new Currency Exchange Informer, updated Readme, env_init.sh script, requirements.txt (#3)
    * fixed typo - added calling of 'message.reply' method (#4)

--- lambda_function.py
+++ lambda_function.py
@@ -1,21 +1,23 @@
 import json, logging, os, requests
 from aiogram import Bot, Dispatcher, executor, types
-from bs4 import BeautifulSoup
 from random import randint
+from bs4 import BeautifulSoup
 
-API_TOKEN=os.environ.get('TOKEN')
-
+# =====================================================================================================================
+# = VARIABLES, LOGGING ================================================================================================
+API_TOKEN = os.environ.get('TOKEN')
 log = logging.getLogger(__name__)
 log.setLevel(os.environ.get('LOGGING_LEVEL', 'INFO').upper())
 
-
+# Here you can override the token to use the bot for debugging - don't forget to roll-back that
+# API_TOKEN = 'someTokenThere'
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
 message_chat_id = -1
 polls_db = {}  # save poll data results here
 
-# Initialize bot and dispatcher
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
-
+# =====================================================================================================================
+# = THIS COMMANDS CONFIGURED FOR BOT INSIDE TELEGRAM BY THE BOTFATHER =================================================
 command_list = '''
 /start - Иди в баню!?
 /help - Начальника сафсем глупый! Слова волшебные знать надо!
@@ -35,20 +37,45 @@ async def send_welcome(message: types.Message):
     message_chat_id = message.chat.id
     await message.reply(f" Даров, {message.from_user.first_name}!\n Вот слова настоящего начальника:\n{command_list}\n")
 
+
 @dp.message_handler(commands=['help'])
 async def send_welcome(message: types.Message):
     message_chat_id = message.chat.id
     await message.reply(f" Начальника, дафай слофа говори:\n{command_list}\n")
 
-jason_geo = ['ща...5 минут турецкий (с)', 'работу работает! .... не то что вы - тунеядцы!', 'позовите Жасона!'
-             , 'в пути...', 'скоро будет...', 'подождем его!']
+
+jason_geo = ['ща...5 минут турецкий (с)',
+             'работу работает! .... не то что вы - тунеядцы!',
+             'позовите Жасона!',
+             'в пути...',
+             'скоро будет...',
+             'подождем его!',
+             'только он знает толк в кальянах!',
+             'ничего без него не можете!',
+             'ожидайте! вас много - а он один!',
+             'точные координаты Жасона дорогого стОят',
+             'Жасон, обнови статус!',
+             ]
+
+
+def get_random_jason_get():
+    return jason_geo[randint(0, len(jason_geo) - 1)]
+
+
 @dp.message_handler(commands=['jason'])
 async def send_welcome(message: types.Message):
-    await message.reply(f"Где Жасон? (с)\n{jason_geo[randint(0, len(jason_geo) - 1)]}")
+    await message.reply(f"Где Жасон? (с)\n{get_random_jason_get()}")
+
+
+@dp.message_handler(regexp='(((J|j)(a|A|)son)|((Д|д|)(Ж|ж|)(а|А|Е|е)(Й|й|)(сон)))')
+async def jason_mantion(message: types.Message):
+    await message.reply(text=f"а где Жасон!? (с)\n{get_random_jason_get()}")
+
 
 @dp.message_handler(commands=['how_much'])
 async def how_much(message: types.Message):
     await message.reply("Сколько за баню?")
+
 
 def get_latest_coin_data(coin_id: str):
     coin_response = requests.get(f"https://api.coinpaprika.com/v1/coins/{coin_id}/ohlcv/today")
@@ -57,31 +84,61 @@ def get_latest_coin_data(coin_id: str):
     coin_price_delta_per_day = float(coin_response.json()[0]['close']) - float(coin_response.json()[0]['open'])
     return f" * {coin_name} : {coin_date}\nUSD {coin_response.json()[0]['close']:.2f} (дельта за день: USD {coin_price_delta_per_day:.2f})\n"
 
+
 @dp.message_handler(commands=['crypt'])
 async def crypt(message: types.Message):
-    await message.reply(f"Почем крипта для народа?\n\n{get_latest_coin_data('btc-bitcoin')}\n{get_latest_coin_data('eth-ethereum')}")
+    await message.reply(
+        f"Почем крипта для народа?\n\n{get_latest_coin_data('btc-bitcoin')}\n{get_latest_coin_data('eth-ethereum')}")
 
-@dp.message_handler(regexp='(crypt|Crypt|CRYPT|крипт|Крипт|КРИПТ|биток|Биток|БИТОК|иткоин|ИТКОИН|Эфир|эфир|ЭФИР|itcoin|ITCOIN|thereum|THEREUM)')
+
+@dp.message_handler(
+    regexp='(crypt|Crypt|CRYPT|крипт|Крипт|КРИПТ|биток|Биток|БИТОК|иткоин|ИТКОИН|Эфир|эфир|ЭФИР|itcoin|ITCOIN|thereum|THEREUM)')
 async def crypt_mantion(message: types.Message):
     await crypt(message)
 
-@dp.message_handler(regexp='(((J|j)(a|A|)son)|((Д|д|)(Ж|ж|)(а|А|Е|е)(Й|й|)(сон)))')
-async def jason_mantion(message: types.Message):
-    await message.reply(text="а где Жасон!? (с)")
 
 @dp.message_handler(regexp='(((W|w)(eather|EATHER))|((П|п)(огод|ОГОД)))')
 async def pogoda_mantion(message: types.Message):
     await pogoda(message)
 
-@dp.message_handler(regexp='(((J|j)(oke|OKE))|((Ш|ш)(ут(ей|)к|УТ(ЕЙ|)К))|(чак|Чак|ЧАК|Chuck|CHUCK|chuck)|(NOR|Nor|nor))')
+
+@dp.message_handler(
+    regexp='(((J|j)(oke|OKE))|((Ш|ш)(ут(ей|)к|УТ(ЕЙ|)К))|(чак|Чак|ЧАК|Chuck|CHUCK|chuck)|(NOR|Nor|nor))')
 async def joke_mantion(message: types.Message):
     await joke_chuck_norris(message)
+
 
 @dp.message_handler(regexp='(((F|f)(act|ACT))|((Ф|ф)(акт|АКТ)))')
 async def fact_mantion(message: types.Message):
     await say_something_useless(message)
 
-# =====================================================
+
+@dp.message_handler(regexp='((К|к)урс)|((Д|д)оллар)|((Б|б)акс)|((Е|е)вро)|((В|в)алют)|((О|о)бменк)')
+async def currency_exchange_mantion(message: types.Message):
+    resp = requests.get("https://kurs.com.ua/ajax/organizationsTable?"
+                        "type=cash"
+                        "&currency_from=1"
+                        "&organizations=pov"
+                        "&city=1217"
+                        "&show_optimal=1"
+                        "&page=money.city"
+                        "&initial_request=1"
+                        "&current_page=money.city")
+    soup = BeautifulSoup(resp.json()['view'], "html.parser")
+    title = soup.h2.get_text(strip=True)
+    headers = [tag.get('title').split(':')[1].strip().upper() if tag.get('title') else '' for tag in
+               soup.find_all('th')]
+    rows = [[cell.get_text("|", strip=True) for cell in row.find_all('td')] for row in soup.tbody.find_all('tr')]
+    res = []
+    res.append(f' ***: {title}:')
+    for row in rows:
+        res.append('-------------------------')
+        for idx, header in enumerate(headers):
+            if header:
+                res.append(f'{header}: {row[idx]}')
+    await message.reply("\n".join(res))
+
+
 @dp.message_handler(commands=['joke_chuck_norris', 'story_about'])
 async def joke_chuck_norris(message: types.Message):
     resp = requests.get("https://api.chucknorris.io/jokes/random")
@@ -90,6 +147,7 @@ async def joke_chuck_norris(message: types.Message):
     source_txt_lang = f"{detect_txt_language(origin_text)}"
     msg = f"\n * {origin_text.strip()}\n... а вот корявый перевод гугло-транслейтом на русский:\n * {translate_to_rus(origin_text, source=source_txt_lang)}"
     await message.reply(f"Расскажи шутку про Чака Норриса:\n{msg}\n{joke_url}")
+
 
 @dp.message_handler(commands=['fact'])
 async def say_something_useless(message: types.Message):
@@ -100,31 +158,34 @@ async def say_something_useless(message: types.Message):
     msg = f"\n ** язык оригинала '{source_txt_lang.upper()}':\n * {origin_text.strip()}\n... а вот вам корявый перевод гугло-транслейтом на русский:\n * {translate_to_rus(origin_text, source=source_txt_lang)}"
     await message.reply(f"Расскажи что нить бесполезное:\n{msg}\n")
 
+
 @dp.message_handler(commands=['beer'])
 async def beer(message: types.Message):
     result_msg = await bot.send_poll(message.chat.id, 'Кто будет пивас?'
-                                  , ['Я!', 'Я и Жасон!', 'Дайте 2!']
-                                  , type="regular", is_anonymous=False
-                                  , allows_multiple_answers=False
-                                  , open_period=3600
-                                  , is_closed=False
-                                  , reply_to_message_id=message.message_id
-                                  , allow_sending_without_reply=True
-                                  )
+                                     , ['Я!', 'Я и Жасон!', 'Дайте 2!']
+                                     , type="regular", is_anonymous=False
+                                     , allows_multiple_answers=False
+                                     , open_period=3600
+                                     , is_closed=False
+                                     , reply_to_message_id=message.message_id
+                                     , allow_sending_without_reply=True
+                                     )
     polls_db['beer'] = result_msg.poll.id
+
 
 @dp.message_handler(commands=['kogda'])
 async def kogda(message: types.Message):
     result_msg = await bot.send_poll(message.chat.id, 'Когда идем в баню в следующий раз?'
-                                  , ['пн', 'вт', 'ср', 'чт', 'пт', 'я не пойду!']
-                                  , type="regular", is_anonymous=False
-                                  , allows_multiple_answers=True
-                                  , open_period=0
-                                  , is_closed=False
-                                  , reply_to_message_id=message.message_id
-                                  , allow_sending_without_reply=True
-                                  )
+                                     , ['пн', 'вт', 'ср', 'чт', 'пт', 'я не пойду!']
+                                     , type="regular", is_anonymous=False
+                                     , allows_multiple_answers=True
+                                     , open_period=0
+                                     , is_closed=False
+                                     , reply_to_message_id=message.message_id
+                                     , allow_sending_without_reply=True
+                                     )
     polls_db['kogda'] = result_msg.poll.id
+
 
 @dp.poll_answer_handler()
 async def handle_poll_answer(poll_answer: types.PollAnswer):
@@ -136,6 +197,7 @@ async def handle_poll_answer(poll_answer: types.PollAnswer):
     elif poll_answer.poll_id == polls_db.get('kogda'):
         await bot.send_message(poll_answer.user.id, 'голос учтен.')
 
+
 @dp.message_handler(commands=['pogoda'])
 async def pogoda(message: types.Message):
     # /api/location/search/?lattlong=(latt),(long) 50°01'53.7"N 36°18'23.5"E
@@ -143,7 +205,7 @@ async def pogoda(message: types.Message):
     # location = requests.get("https://www.metaweather.com/api/location/search/?lattlong=50.03,36.31")
     # {"distance": 7214, "title": "Kharkiv", "location_type": "City", "woeid": 922137, "latt_long": "49.990101,36.230301"}
 
-    weather_resp = requests.get("https://www.metaweather.com/api/location/922137/") # /api/location/(woeid)/
+    weather_resp = requests.get("https://www.metaweather.com/api/location/922137/")  # /api/location/(woeid)/
     consolidated_weather = json.loads(weather_resp.content)['consolidated_weather'][0]
     cur_temp = float(consolidated_weather['the_temp'])
     min_temp = consolidated_weather['min_temp']
@@ -152,19 +214,19 @@ async def pogoda(message: types.Message):
     description = f"\n... а если более точно, то температура:\n" \
                   f"   сейчас: {cur_temp:.2f}°C\n   MIN: {min_temp:.2f}°C\n   MAX: {max_temp:.2f}°C\n" \
                   f" Вообщем, погода у нас хорошая! Обращайся! ;)"
-    if ( cur_temp >= 25.0):
+    if (cur_temp >= 25.0):
         await message.reply(f"Погода?\nМожно уже и на морько!\n{description}\n")
-    elif (cur_temp >= 15.0 ):
+    elif (cur_temp >= 15.0):
         await message.reply(f"Погода?\nМожно катать! Седлай мот!\n{description}\n")
-    elif (cur_temp >= 5.0 ):
+    elif (cur_temp >= 5.0):
         await message.reply(f"Погода?\nМожно катать! .... правда, пока только на веле :)\n{description}\n")
-    elif (cur_temp >= 0.0 ):
+    elif (cur_temp >= 0.0):
         await message.reply(f"Погода?\nМожет в баню?!\n{description}\n")
-    elif (cur_temp >= -5.0 ):
+    elif (cur_temp >= -5.0):
         await message.reply(f"Погода?\nБаня будет хороша!\n{description}\n")
-    elif (cur_temp >= -10.0 ):
+    elif (cur_temp >= -10.0):
         await message.reply(f"Погода?\nСаня, хоооолодно! Иди в баню!\n{description}\n")
-    elif (cur_temp >= -15.0 ):
+    elif (cur_temp >= -15.0):
         await message.reply(f"Погода?\nСаня, ппц как холодно! (с)\n{description}\n")
     else:
         await message.reply(f"Погода?\nТам реальный дубарь - никуда не ходи!!!{description}\n")
@@ -189,10 +251,11 @@ def detect_txt_language(txt_in_unknown_lang):
         'accept-language': 'en-US,en;q=0.9',
     }
     data = {
-      'q': txt_in_unknown_lang
+        'q': txt_in_unknown_lang
     }
     response_txt = requests.post('https://libretranslate.com/detect', headers=headers, data=data).text.strip()
     return json.loads(response_txt)[0]['language']
+
 
 def translate_to_rus(txt_to_translate, source='en'):
     headers = {
@@ -213,14 +276,17 @@ def translate_to_rus(txt_to_translate, source='en'):
         'accept-language': 'en-US,en;q=0.9',
     }
     data = {
-      'q': txt_to_translate,
-      'source': source,
-      'target': 'ru',
-      'format': 'text'
+        'q': txt_to_translate,
+        'source': source,
+        'target': 'ru',
+        'format': 'text'
     }
     response_txt = requests.post('https://libretranslate.com/translate', headers=headers, data=data).text
     return json.loads(response_txt)['translatedText'].strip()
 
+
+# =====================================================================================================================
+# = COMMON FUNCTIONS FOR RUNNING THE BOT CODE AS LAMBDA HANDLER OR DIRECT MODULE EXECUTING ============================
 async def process_event(event, dp: Dispatcher):
     """
     Converting an AWS Lambda event to an update and handling that
@@ -231,6 +297,7 @@ async def process_event(event, dp: Dispatcher):
     update = types.Update.to_object(event)
     await dp.process_update(update)
 
+
 async def main(event):
     """
     Asynchronous wrapper for launching processing events.
@@ -238,10 +305,12 @@ async def main(event):
     await process_event(event, dp)
     return 'ok'
 
+
 def lambda_handler(event, context):
     """AWS Lambda handler."""
     import asyncio
     return asyncio.get_event_loop().run_until_complete(main(event))
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
